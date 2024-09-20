59e12c02ad0ee2edc0292c406a036e8e1b026b57
Artur <admin@deswolf.com>
2024-09-20 22:20:04 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Resend } from "resend";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const RESEND_API_KEY = env.RESEND_API_KEY;
+		const fromDomain =  env.FROM_DOMAIN;
+		const defaultValues = {};
+		const fromUsername = "contact-noreply";
+		const fromLabel = "Info";
+		const resend = new Resend(RESEND_API_KEY);
+
+    const { data, error } = await resend.emails.send({
+      from: `${fromLabel} <${fromUsername}@${fromDomain}>`,
+			replyTo: ["anything@dasdsa46.com"],
+      to: "sample@example654.com",
+      subject: "Hello World2",
+      html: "<p>Hello3 from Workers</p>",
+    });
+
+		if (error) {
+			return Response.json({ error }, { status: 400 });
+		}
+
+		return Response.json(data);
 	},
 } satisfies ExportedHandler<Env>;
