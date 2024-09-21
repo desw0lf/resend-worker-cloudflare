446636f36f5969998860b4599b71e0e6a5f26a10
Artur <admin@deswolf.com>
2024-09-21 14:04:17 +0100

    fix setup check

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,7 +1,7 @@
 import { error } from "itty-router";
 
 const authMiddleware = (request: Request, env: Env) => {
-  if (!env.RESEND_API_KEY || !env.CUSTOM_TOKEN || !env.FROM_DOMAIN || env.FROM_DOMAIN.indexOf(".") < 0) {
+  if (!env.RESEND_API_KEY || !env.CUSTOM_TOKEN || !env.RESEND_YOUR_DOMAIN || env.RESEND_YOUR_DOMAIN.indexOf(".") < 0) {
 		return error(401, { description: "INVALID SETUP" });
 	}
 
